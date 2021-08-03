4aa971203f9fe7891e280f9add200cb2240c49a8
TANNER MECK <steve.meck246@gmail.com>
2021-08-03 16:05:36 -0700

    dropdown menus created

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import ImageList from './ImageList.js';
 
 class App extends Component {
   state = { 
-    keyword:'All'
-   }
+    keyword:'all',
+    horns: 'all',
+    sortOption: 'horns'
+  }
+  handleKeywordChange=(e) => {this.setState({keyword: e.target.value, sortOption: 'keyword'})}
+  handleChange=(event) => {this.setState({horns: event.target.value, sortOption: 'horns'})}
   render() { 
+    const filteredImage = images.filter(image => image.keyword === this.state.keyword || this.state.keyword === 'all')
+    const hornsFiltered = images.filter(image => this.state.horns === 'all' || image.horns === Number(this.state.horns))
     return ( 
       <>
         <div className="dropDown">
-          <h1>Choose Your Favorite Creature</h1>
-          <select>
-            <option value="All">All</option>
+          <h1>Choose Your Favorite Creature Category</h1>
+          <select onChange={this.handleKeywordChange}>
+            <option value="all">all</option>
             <option value="narwhal">narwhal</option>
             <option value="rhino">rhino</option>
             <option value="unicorn">unicorn</option>
@@ -27,8 +33,18 @@ class App extends Component {
             <option value="dragon">dragon</option>
           </select>
         </div>
+        <div className="hornsDropdown">
+          <h1>Choose Number of Horns</h1>
+        <select onChange={this.handleChange}>
+            <option value="all">all</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="100">100</option>
+        </select>
+        </div>
         <div>
-          <ImageList Creatures={images}/>
+          <ImageList Creatures={this.state.sortOption === 'keyword' ? filteredImage : hornsFiltered}/>
         </div>
       </>
      );
