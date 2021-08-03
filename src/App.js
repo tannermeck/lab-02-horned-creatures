e26e8c920aff12babc1ee123578f40ab8c5c9dc0
TANNER MECK <steve.meck246@gmail.com>
2021-08-03 14:48:10 -0700

    dropdown created

--- src/App.js
+++ src/App.js
@@ -4,13 +4,33 @@ import images from './Data.js';
 import ImageList from './ImageList.js';
 
 class App extends Component {
-  state = {  }
+  state = { 
+    keyword:'All'
+   }
   render() { 
     return ( 
-      <div>
-        <h1>Choose Your Favorite Creature</h1>
-        <ImageList Creatures={images}/>
-      </div>
+      <>
+        <div className="dropDown">
+          <h1>Choose Your Favorite Creature</h1>
+          <select>
+            <option value="All">All</option>
+            <option value="narwhal">narwhal</option>
+            <option value="rhino">rhino</option>
+            <option value="unicorn">unicorn</option>
+            <option value="unilego">unilego</option>
+            <option value="triceratops">triceratops</option>
+            <option value="markhor">markhor</option>
+            <option value="mouflon">mouflon</option>
+            <option value="addax">addax</option>
+            <option value="chameleon">chameleon</option>
+            <option value="lizard">lizard</option>
+            <option value="dragon">dragon</option>
+          </select>
+        </div>
+        <div>
+          <ImageList Creatures={images}/>
+        </div>
+      </>
      );
   }
 }
